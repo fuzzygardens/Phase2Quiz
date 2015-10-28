582ae932d9fc11fe4c870280c4da09d6b275c6da
Conor Yuen <conor.yuen16@trinityschoolnyc.org>
2015-10-28 13:36:20 -0400

    new changes

---
+++ server.js
@@ -0,0 +1,16 @@
+var fs =require('fs');
+
+var express = require('express');
+var app = express();
+app.use('/Use', express.static(__dirname + '/Use'));
+
+app.get('/', function (req, res) {
+  var content = fs.readFileSync("Use/index.html", 'utf8');
+  res.send(content);
+});
+
+app.get('/quiz', function (req, res) {
+  res.send('Hello World! New Stuff!');
+});
+
+var server = app.listen(3000);
